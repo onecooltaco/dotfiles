c77bf012061709ed610c9f06c3648d5cd9fd1453
Jeremy Leggat <jleggat@asu.edu>
2025-04-28 13:04:18 -0700

    Install MacVim with HomeBrew if not present
    Update vim-plugins.sh

--- vim-plugins.sh
+++ vim-plugins.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 set -eu
 
-mkdir -p ~/.vim/backups
-mkdir -p ~/.vim/swaps
-mkdir -p ~/.vim/pack/plugins/start
+ROOT_DIR="$(dirname -- "$(stat -f "$0")")"
 
-git clone https://github.com/catppuccin/vim.git ~/.vim/pack/plugins/start/catppuccin-vim
-git clone https://github.com/aklt/plantuml-syntax.git ~/.vim/pack/plugins/start/plantuml-syntax
-git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/pack/local/start/editorconfig-vim
-git clone https://github.com/hashivim/vim-terraform.git ~/.vim/pack/plugins/start/vim-terraform
+if ! mdfind -name 'MacVim.app' &>/dev/null; then
+  brew install --cask macvim
+fi
+
+for f in gvimrc vimrc; do
+	[[ -f ${HOME}/.${f} ]] && cp ${HOME}/.${f}{,~}
+	cp "${ROOT_DIR}/${f}" ${HOME}/.${f}
+done
+
+for d in backups swaps pack/plugins/start; do
+  mkdir -p ${HOME}/.vim/${d}
+done
+
+plugins_array=("catppuccin/vim" "aklt/plantuml-syntax" "editorconfig/editorconfig-vim" "hashivim/vim-terraform")
+
+for p in "${plugins_array[@]}"; do
+  dir="${HOME}/.vim/pack/plugins/start/${p//\//_}"
+  if [ ! -d "$dir" ]; then
+    echo "downloading Vim Plugin ${p}..."
+    git clone https://github.com/${p}.git ${dir}
+  else
+    echo "Vim Plugin ${p} already installed."
+  fi
+done
