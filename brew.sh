dac3fa2c183e28e13cb19b60692202e1ce32a581
Jeremy Leggat <jleggat@asu.edu>
2026-05-20 12:21:06 -0700

    fix(homebrew): update brew install url

--- brew.sh
+++ brew.sh
@@ -13,7 +13,7 @@ fi
 # Install if needed
 if test ! "$(which brew)"; then
   echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # update brew packages
