8e21d2ce28e11e737e109279887fc5d5ebafcabf
Jeremy Leggat <jleggat@asu.edu>
2026-05-20 17:17:38 -0700

    Update iterm2.sh

--- iterm2.sh
+++ iterm2.sh
@@ -12,18 +12,18 @@ fi
 
 mkdir "${TEMP_DIR}/schemes"
 mkdir "${TEMP_DIR}/tools"
-curl -Ls https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/tools/import-scheme.sh > "${TEMP_DIR}/tools/import-scheme.sh"
+curl -Ls https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/refs/heads/master/tools/import-scheme.sh > "${TEMP_DIR}/tools/import-scheme.sh"
 
 catppuccin_array=("frappe" "latte" "macchiato" "mocha")
 
 for f in "${catppuccin_array[@]}"
 do
   echo "downloading catppuccin-${f}.itermcolors file..."
-  curl -Ls "https://raw.githubusercontent.com/catppuccin/iterm/main/colors/catppuccin-${f}.itermcolors" > "${TEMP_DIR}/schemes/catppuccin-${f}.itermcolors"
+  curl -Ls "https://raw.githubusercontent.com/catppuccin/iterm/refs/heads/main/colors/catppuccin-${f}.itermcolors" > "${TEMP_DIR}/schemes/catppuccin-${f}.itermcolors"
   echo "Processing catppuccin-${f}.itermcolors file..."
   # Import file $f
-  sh "${TEMP_DIR}/tools/import-scheme.sh catppuccin-${f}.itermcolors"
+  sh "${TEMP_DIR}/tools/import-scheme.sh" "${TEMP_DIR}/schemes/catppuccin-${f}.itermcolors"
 done
 
 defaults write com.googlecode.iterm2 QuitWhenAllWindowsClosed -bool true
-cp -f "${ROOT_DIR}/iterm-profiles.json" "${HOME}/Library/Application\ Support/iTerm2/DynamicProfiles/"
+cp -f "${ROOT_DIR}/iterm-profiles.json" "${HOME}/Library/Application Support/iTerm2/DynamicProfiles/"
